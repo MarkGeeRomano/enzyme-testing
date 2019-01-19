04629effac5ad126e9c605e16d980d56820d2266
mark <mark@marks-MacBook-Pro.local>
2019-01-19 13:01:37 -0500

    beginning to add redux tests

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -4,7 +4,7 @@ import CommentBox from 'components/CommentBox'
 
 let wrapped
 beforeEach(() => {
-  wrapped = mount(<CommentBox/>)
+  wrapped = mount(<CommentBox />)
 })
 
 afterEach(() => wrapped.unmount())
@@ -12,4 +12,22 @@ afterEach(() => wrapped.unmount())
 it('has a text area and buttster', () => {
   expect(wrapped.find('button').length).toEqual(1)
   expect(wrapped.find('textarea').length).toEqual(1)
+})
+
+describe('the text area', () => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', { target: { value: 'hehe' } })
+    wrapped.update()
+  })
+
+  it('has a textarea ppl can writ in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('hehe')
+  })
+  
+  it('dumps text when submitted', () => {
+    wrapped.find('button').simulate('submit')
+    wrapped.update()
+    
+    expect(wrapped.find('textarea').prop('value')).toEqual('')
+  })
 })
